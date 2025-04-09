77624d14ed8fbcec28b55c5c99a75daebefc7512
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2025-04-09 14:47:16 +0100

    feat: get user input

--- src/app/action.ts
+++ src/app/action.ts
@@ -1,7 +1,20 @@
 'use server'
 
-export async function checkUserInputAction(form: FormData){
-  'use server'
-  const userInput = form.get('userInput[0][0]') as string;
-  console.log({userInput });
+function extractUserInputFromFormData(form: FormData, rows: number, cols: number): string[][] {
+  const userInput: string[][] = [];
+  for (let i = 0; i < rows; i++) {
+    const row: string[] = [];
+    for (let j = 0; j < cols; j++) {
+      const value = form.get(`userInput[${i}][${j}]`) as string;
+      row.push(value);
+    }
+    userInput.push(row);
+  }
+  return userInput;
+}
+
+export async function checkUserInputAction(form: FormData, rows: number, cols: number) {
+  const userInput = extractUserInputFromFormData(form, rows, cols);
+  console.log({userInput, });
+  return userInput;
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,10 @@ const COLS= 5;
 export default async function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <form action={checkUserInputAction}>
+      <form action={async(form)=>{
+        'use server'
+        return await checkUserInputAction(form,ROWS,COLS);
+      }} >
       <InputBoxes rows={ROWS} cols={COLS} />
         <button type="submit" className="mt-4 px-4 py-2 bg-blue-500 text-white rounded">
           submit!
