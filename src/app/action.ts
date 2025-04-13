ef341b3ff05fc467f540c2948b604552c1885b46
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2025-04-13 18:22:04 +0100

    feat: disabled on non-active rows

--- src/app/action.ts
+++ src/app/action.ts
@@ -14,6 +14,7 @@ function extractUserInputFromFormData(form: FormData, rows: number, cols: number
     const row: string[] = [];
     for (let j = 0; j < cols; j++) {
       const value = form.get(`userInput[${i}][${j}]`) as string;
+      console.log(value)
       row.push(value);
     }
     userInput.push(row);
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -7,6 +7,7 @@ interface InputBoxesProps {
   rows: number;
   cols: number;
   status: WordleStatusArray;
+  userInput: string[][];
 }
 /**
 * create a input boxes like wordle
@@ -14,17 +15,28 @@ interface InputBoxesProps {
 function InputBoxes({
   rows,
   cols,
-  status
+  status,
+  userInput,
 }: InputBoxesProps) {
+  let activeRow = 0;
+  for (const row of status) {
+    if (row.some((cell) => cell === '')) {
+      break;
+    }
+    activeRow++;
+  }
+  console.log("activeRow", activeRow);
   return (
     <div className="flex flex-col gap-2">
       {Array.from({ length: rows }).map((_, rowIndex) => (
         <div key={rowIndex} className="flex gap-2">
           {Array.from({ length: cols }).map((_, colIndex) => (
-            < InputBox
+            <InputBox
+              disabled={rowIndex !== activeRow}
               key={colIndex}
               name={`userInput[${rowIndex}][${colIndex}]`}
               status={status.at(rowIndex)?.at(colIndex)}
+              value={userInput.at(rowIndex)?.at(colIndex)}
             />
           ))}
         </div>
@@ -36,20 +48,28 @@ function InputBoxes({
 interface InputBoxProps {
   status?: WordleStatus
   name: string;
+  disabled?: boolean;
+  value?: string;
 }
-function InputBox({ status, name,  }: InputBoxProps) {
+function InputBox({ status, name, disabled, value}: InputBoxProps) {
+  /** when we use `disabled` prop, the form will not send the value of this input. therefore we need to use readOnly instead */
   return (
     <input
       type="text"
+      readOnly={disabled}
       name={name}
       maxLength={1}
+      defaultValue={value}
       className={clsx(
         "w-12 h-12 text-center border rounded focus:outline-none focus:ring-2 focus:ring-blue-500",
         {
+          "bg-white": status === '',
           "bg-gray-200": status === 'absent',
           "bg-green-500": status === 'correct',
           "bg-yellow-500": status === 'present',
-        }
+          "bg-neutral-100": disabled,
+        },
+        disabled && "cursor-not-allowed",
       )}
     />
   );
@@ -73,7 +93,7 @@ export async function Form({
       'use server'
       await submitInputAction(form,ROWS,COLS, targetWord);
     }} >
-      <InputBoxes rows={ROWS} cols={COLS} status={status} />
+      <InputBoxes rows={ROWS} cols={COLS} status={status} userInput={userInput} />
       <button type="submit" className="mt-4 px-4 py-2 bg-blue-500 text-white rounded">
         submit!
       </button>
